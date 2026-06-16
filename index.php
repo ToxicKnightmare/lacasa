cfa16b8fb70b08809b5312e174ea7e58049d46ce
Lance Mayfield <toxicknightmare@outlook.com>
2026-06-16 15:25:38 -0700

    First Push then will create a Branch

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>La Casa Argentina - Alamos Sonora Mexico</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="assets/css/bootstrap-icons.css"/>
+        <link rel="stylesheet" href="assets/css/style.css"/>
+
+        <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script> 
+
+    </head>
+    <body>
+        <div class="page-wrapper">
+            <?php
+                include 'containers/header.html';
+                include 'containers/hero.html';
+            ?>
+        </div>
+    </body>
+</html>
